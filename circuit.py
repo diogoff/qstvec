3d623ebc48bc4bd406bb1294d13ac4221af0ded4
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2026-03-22 08:43:30 +0000

    working on circuit dag

--- circuit.py
+++ circuit.py
@@ -1,13 +1,8 @@
-import os
 import sys
-import math
-import time
-import psutil
+import numpy as np
 
 from qiskit import QuantumCircuit
-from qiskit.quantum_info import Operator
-
-from qssvec import SparseStatevector
+from qiskit.converters import circuit_to_dag
 
 # -----------------------------------------------------------------------------
 
@@ -17,88 +12,24 @@ if len(sys.argv) < 2:
 
 qc = QuantumCircuit.from_qasm_file(sys.argv[1])
 
-qc_size = qc.size()
-
 # -----------------------------------------------------------------------------
 
-layers = []
-last_layer_i = [-1] * qc.num_qubits
+dag = circuit_to_dag(qc)
 
-for ci in qc.data:
-    qset = set([qc.find_bit(q).index for q in ci.qubits])
+seen = set()
+scheduled = []
 
-    i0 = 0
-    for q in qset:
-        if last_layer_i[q] > i0:
-            i0 = last_layer_i[q]
+while dag.op_nodes():
+    front = dag.front_layer()
 
-    best_overlap = 0
-    best_layer_i = -1
+    candidates = []
+    for node in front:
+        qinds = [dag.find_bit(q).index for q in node.qargs]
+        new_q = [q for q in qinds if q not in seen]
+        candidates.append((len(new_q), -len(qinds) + len(new_q), node._node_id, node, qinds))
 
-    for i in range(i0, len(layers)):
-        overlap = len(layers[i][0] & qset)
-        if overlap > best_overlap:
-            best_overlap = overlap
-            best_layer_i = i
 
-    if best_overlap > 0:
-        i = best_layer_i
-        layers[i][0] |= qset
-        layers[i][1] += [ci]
-    else:
-        i = len(layers)
-        layer = [qset, [ci]]
-        layers.append(layer)
+
+
     
-    for q in qset:
-        if i > last_layer_i[q]:
-            last_layer_i[q] = i
-
-
-for i, layer in enumerate(layers):
-    print(f'layer {i+1} | qset {' '.join(map(str, layer[0]))} | gates {len(layer[1])}')
-
-exit()
-
-# -----------------------------------------------------------------------------
-
-sv = SparseStatevector(qc.num_qubits)
-
-ci_num = 0
-
-t0 = time.perf_counter()
-
-for layer in layers:
-    for ci in layer[1]:
-        ci_num += 1
-
-        name = ci.operation.name
-        params = ci.operation.params
-        qargs = [qc.find_bit(q).index for q in ci.qubits]
-        qargs = [qc.find_bit(q).index for q in ci.qubits]
-
-        U = Operator(ci.operation).data
-        sv = sv.evolve(U, qargs)
-        
-        p_frac = 0.999
-        sv = sv.truncate(p_frac)
-        n = len(sv.data)
-    
-        mag = sv.data.abs()
-        idx = mag.idxmax()
-        bit = f'{idx:0{qc.num_qubits}b}'
-        mag = mag[idx]
-
-        pid = psutil.Process(os.getpid())
-        mem = pid.memory_info().rss / 1024**3
-
-        t1 = time.perf_counter()
-
-        print()
-        print(f'{ci_num}/{qc_size} | {ci_num/qc_size*100.:.1f}%')
-        print(f'{name} | {' '.join(map(str, params))} | qubit {' '.join(map(str, qargs))}')
-        print(f'{bit} | mag {mag:.3e}')
-        print(f'{n} terms | order 2^{int(math.ceil(math.log2(n)))} | mem {mem:.1f} GB')
-        print(f'{t1-t0:.1f} s/it | {(t1-t0)*(qc_size-ci_num)/3600:.1f} hours')
-
-        t0 = t1
+    break
