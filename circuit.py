568f3762515a410267b1471af0996a1d6e8ec1a3
Diogo R. Ferreira <diogo.ferreira@tecnico.ulisboa.pt>
2026-03-23 19:12:23 +0000

    working on circuit

--- circuit.py
+++ circuit.py
@@ -1,7 +1,8 @@
 import sys
-import numpy as np
+import copy
 
 from qiskit import QuantumCircuit
+from qiskit.circuit import CircuitInstruction
 from qiskit.converters import circuit_to_dag
 
 # -----------------------------------------------------------------------------
@@ -14,22 +15,67 @@ qc = QuantumCircuit.from_qasm_file(sys.argv[1])
 
 # -----------------------------------------------------------------------------
 
-dag = circuit_to_dag(qc)
+output_seq = []
 
-seen = set()
-scheduled = []
+def traverse(current_dag, current_qset, current_seq):
+    if current_dag.size() == 0:
+        if not output_seq:
+            output_seq = copy.deepcopy(current_seq)
+        else:
+            better = False
+            for i in range(current_seq):
+                if current_seq[i][0] < output_seq[i][0]:
+                    better = True
+                    break
+            if better:
+                output_seq = copy.deepcopy(current_seq)
+        return
 
-while dag.op_nodes():
-    front = dag.front_layer()
+    while True:
+        front_nodes = dict()
+        for node in current_dag.front_layer():
+            qset = frozenset(node.qargs)
+            if qset not in front_nodes:
+                front_nodes[qset] = []
+            front_nodes[qset].append(node)
 
-    candidates = []
-    for node in front:
-        qinds = [dag.find_bit(q).index for q in node.qargs]
-        new_q = [q for q in qinds if q not in seen]
-        candidates.append((len(new_q), -len(qinds) + len(new_q), node._node_id, node, qinds))
+        if len(front_nodes) == 0:
+            output_seqs.append(current_seq)
+            return
+
+        ready_nodes = dict()
+        for qset in front_nodes:
+            if qset <= current_qset:
+                ready_nodes[qset] = front_nodes[qset]
+
+        if len(ready_nodes) == 0:
+            break
+
+        for qset in ready_nodes:
+            for node in ready_nodes[qset]:
+                current_dag.remove_op_node(node)
+                current_seq.append((qset, node))
+
+    for qset in front_nodes:
+        new_current_qset = current_qset | qset
+        new_current_dag = copy.deepcopy(current_dag)
+        new_current_seq = copy.deepcopy(current_seq)
+        for node in front_nodes[qset]:
+            new_current_dag.remove_op_node(node)
+            new_current_seq.append((qset, node))
+        traverse(new_current_dag, new_current_qset, new_current_seq, output_seq)
+
+# -----------------------------------------------------------------------------
 
 
+for q in range(qc.num_qubits):
+    current_dag = circuit_to_dag(qc)
+    current_qset = frozenset({current_dag.qubits[q]})
+    current_seq = []
+    traverse(current_dag, current_qset, current_seq, output_seq)
 
-
-    
-    break
+print(f'Sequence with {len(output_seq)} instructions:')
+for i, node in enumerate(output_seq):
+    name = node.op.name
+    qind = [qc.find_bit(q).index for q in node.qargs]
+    print(f'{i+1}) {node.op.name} {' '.join(map(str, qind))}')
