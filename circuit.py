b2975b503e10704b1e91959758ac3bf0669bf097
Diogo R. Ferreira <diogo.ribeiro.ferreira@gmail.com>
2026-03-25 07:23:19 +0000

    n_max

--- circuit.py
+++ circuit.py
@@ -55,7 +55,7 @@ for i, node in enumerate(seq, start=1):
         U = Operator(node.op).data
         qargs = [qc.find_bit(q).index for q in node.qargs]
 
-        sv.evolve(U, qargs).truncate(0.999, int(1e8))
+        sv.evolve(U, qargs).truncate(0.999, 2**26)
         b_str, prob = sv.bit_string(return_prob=True)
 
         t1 = time.perf_counter()
